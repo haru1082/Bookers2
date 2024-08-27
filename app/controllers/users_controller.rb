93421bd9879b600b5c4468ae74f3f785e7cff7d7
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-08-27 20:49:13 +0000

    User info作成中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   def index
-  end 
+    @users = User.page(params[:page])
+  end
 
   def show
     @user = User.find(params[:id])
@@ -11,18 +12,18 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
-  end 
-  
+  end
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
-  end 
-  
+  end
+
 
 end
