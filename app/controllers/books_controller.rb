11e190611170a5aec55eecaf8e1262b196898e57
EC2 Default User <ec2-user@ip-172-31-19-10.ap-northeast-1.compute.internal>
2024-08-28 17:41:01 +0000

    エラー対応中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -16,8 +16,10 @@ class BooksController < ApplicationController
    @book = Book.new(book_params)
    @book.user_id = current_user.id
   if @book.save
+   flash[:notice] = "Book was successfully created"
    redirect_to book_path(@book.id)
   else
+   flash.now[:alert] = "error:Posting failed"
    render :new
   end
  end 
